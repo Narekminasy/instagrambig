cd8d3b4afaeab28eef1e4f1391978ec97a703735
Narek Minasyan <narekminasyan52@gmail.com>
2026-09-03 14:39:04 +0400

    change upload logic

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,16 +1,24 @@
 import multer from "multer";
 import mime from "mime";
-import {v4 as UUIDv4} from "uuid";
+import { v4 as UUIDv4 } from "uuid";
+import fs from "fs";
+import path from "path";
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "public/media");
+        const dir = path.resolve("public/media");
+
+        if (!fs.existsSync(dir)) {
+            fs.mkdirSync(dir, { recursive: true });
+        }
+
+        cb(null, dir);
     },
     filename: (req, file, cb) => {
-        cb(null, UUIDv4() + '.' + mime.getExtension(file.mimetype));
+        cb(null, UUIDv4() + "." + mime.getExtension(file.mimetype));
     }
-})
+});
 
-const upload =multer({ storage: storage });
+const upload = multer({ storage: storage });
 
-export default upload
+export default upload;
