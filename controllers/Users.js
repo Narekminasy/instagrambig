b3dd23cdd4872c8272f4765545dde527cd0f2f14
Narek Minasyan <narekminasyan52@gmail.com>
2026-09-03 15:45:59 +0400

    fixed errors

--- controllers/Users.js
+++ controllers/Users.js
@@ -8,8 +8,8 @@ import nodemailer from 'nodemailer';
 
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
-    port: parseInt(process.env.SMTP_PORT, 10) || 587,
-    secure: process.env.SMTP_PORT == 465,
+    port: parseInt(process.env.SMTP_PORT, 10) || 465,
+    secure: true,
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
